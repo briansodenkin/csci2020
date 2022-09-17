20f9962350c9f29bcac727d1778af417de40c7a1
briansodenkin <66714154+briansodenkin@users.noreply.github.com>
2022-09-18 00:51:16 +0800

    Create q2.py

---
+++ lab1/q2.py
@@ -0,0 +1,17 @@
+while True:
+    print("Enter an integer:", end=" ")
+    rows = int(input())
+    if rows == 0:
+        break
+    print("Enter a string:", end=" ")
+    out = str(input())
+
+    for i in range(1, rows + 1):
+        for j in range(rows, 0, -1):
+            if j > i:
+                for _ in range(len(out)):
+                    print(" ", end='')
+            else:
+                print(str(out), end='')
+        print("")
+print("Program ends.")
