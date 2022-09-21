415a78528a9ae33de804d608088c7c5936c50b8b
briansodenkin <66714154+briansodenkin@users.noreply.github.com>
2022-09-21 23:35:03 +0800

    Update p3.py

--- lab1/p3.py
+++ lab1/p3.py
@@ -1,17 +1,17 @@
 while True:
     print("Enter an integer:", end=" ")
-    rows = int(input())
-    if rows == 0 or rows < 0:
+    rows_num = int(input())
+    if rows_num == 0 or rows_num < 0:
         break
     print("Enter a string:", end=" ")
-    out = str(input())
-
-    for i in range(1, rows + 1):
-        for j in range(rows, 0, -1):
+    string = str(input())
+    for i in range(0, rows_num):
+        for j in range(rows_num, 0, -1):
             if j > i:
-                for _ in range(len(out)):
+                # print number of space according to the string length
+                for _ in range(len(string)):
                     print(" ", end='')
             else:
-                print(str(out), end='')
+                print(str(string), end='')
         print("")
 print("Program ends.")
