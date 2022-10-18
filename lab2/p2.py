40360bb261cec7b0c86099646e7eb642a4b50347
briansodenkin <66714154+briansodenkin@users.noreply.github.com>
2022-10-18 20:22:11 +0800

    Create p2.py

---
+++ lab2/p2.py
@@ -0,0 +1,20 @@
+def roman_to_decimal(str1, str2):
+    roman_int_map = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    int1, int2 = 0, 0
+	
+    for i in range(len(str1)):
+        if i > 0 and roman_int_map[str1[i]] > roman_int_map[str1[i - 1]]:
+            int1 = int1 + roman_int_map[str1[i]] - 2 * roman_int_map[str1[i - 1]]
+        else:
+            int1 = int1 + roman_int_map[str1[i]]
+
+    for i in range(len(str2)):
+        if i > 0 and roman_int_map[str2[i]] > roman_int_map[str2[i - 1]]:
+            int2 = int2 + roman_int_map[str2[i]] - 2 * roman_int_map[str2[i - 1]]
+        else:
+            int2 = int2 + roman_int_map[str2[i]]
+    n = min(int1, int2)
+    return n
+
+
+print(roman_to_decimal('CIDMLV', 'DXCVI'))
