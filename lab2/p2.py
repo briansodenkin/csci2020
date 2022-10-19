17724d974101d98129c37c5176aeb3bf02a13816
briansodenkin <66714154+briansodenkin@users.noreply.github.com>
2022-10-19 18:48:37 +0800

    Update p2.py

--- lab2/p2.py
+++ lab2/p2.py
@@ -1,9 +1,12 @@
 def roman_to_decimal(str1, str2):
+	# value mapping provided
     roman_int_map = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     int1, int2 = 0, 0
-	
     for i in range(len(str1)):
+        # if not the leading character, and current number
         if i > 0 and roman_int_map[str1[i]] > roman_int_map[str1[i - 1]]:
+            # minus the value again
+            # e.g. CM, 1100 - 100 * 2
             int1 = int1 + roman_int_map[str1[i]] - 2 * roman_int_map[str1[i - 1]]
         else:
             int1 = int1 + roman_int_map[str1[i]]
@@ -15,6 +18,3 @@ def roman_to_decimal(str1, str2):
             int2 = int2 + roman_int_map[str2[i]]
     n = min(int1, int2)
     return n
-
-
-print(roman_to_decimal('CIDMLV', 'DXCVI'))
